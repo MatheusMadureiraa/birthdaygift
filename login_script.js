4ea144b287a9956f8bc4c20289f07e0b8387f3dd
Matheus Madureira <143838250+MatheusMadureiraa@users.noreply.github.com>
2024-07-01 21:38:35 -0300

    Update login_script.js

--- login_script.js
+++ login_script.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const password = document.getElementById('password').value;
 
     if (username === 'manuella' && password === '02072005') {
-        window.location.href = 'birthday_page.html';
+        window.location.href = 'text.html';
     } else {
         document.getElementById('errorMessage').textContent = 'Username ou senha incorretos!';
         document.getElementById('errorMessage').style.display = 'block';
